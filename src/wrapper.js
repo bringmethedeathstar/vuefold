47519b0fa582b4f83926ac9198d980f1ab0a5904
Dave <dave.p@formationmedia.co.uk>
2019-12-13 15:25:26 +0000

    Prettier

---
+++ src/wrapper.js
@@ -0,0 +1,25 @@
+import VueFold from './VueFold.vue';
+
+export function install(Vue) {
+  if (install.installed) return;
+
+  install.installed = true;
+
+  Vue.component('VueFold', VueFold);
+}
+
+const plugin = { install };
+
+let GlobalVue = null;
+
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue;
+}
+
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+export default VueFold;
